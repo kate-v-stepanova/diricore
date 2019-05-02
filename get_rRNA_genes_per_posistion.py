be1bb3e17fe3e4e6b77c9b404b7f7aba840523bc
b250-admin <b250-admin@dkfz-vpn40.inet.dkfz-heidelberg.de>
2019-05-02 12:32:59 +0200

    rRNA positions (--amend)

--- get_rRNA_genes_per_posistion.py
+++ get_rRNA_genes_per_posistion.py
@@ -49,35 +49,30 @@ for infile in blat_results:
     # a line looks like: 345202_TTCGCGCGGGTCGGGGGGCGGGGCGGACTGT 18 8 25 31 100.0 chr6_cox_hap2 - 4700634 4700651 18 None None
     # we only need our id (which is counts+sequence), then chromosome, start and end positions
     df = pd.read_csv(infile, sep=" ", header=None, usecols=[0,6,8,9])
-    df.columns = ['counts_sequence', 'chrom', 'start', 'end']
+    df.columns = ['counts_sequence', 'gene', 'start', 'end']
+    df[['counts', 'sequence']] = df.get('counts_sequence').str.split('_', expand=True)
+    df['gene'] = df['gene'].str.split('__').str[-1]
+    df['read_length'] = df['sequence'].str.len()
+    df['reads_info'] = ''
 
-    # filter results (keep only the hits that aligned to chromosomes of rRNA genes)
-    df = df[df.get('chrom').isin(chromosomes)]
-    print(len(df))
-    # keep only the first hits (the sequences can be aligned to multiple regions, and blat outputs all of them)
-    # "counts_sequence" column contains sequence which was aligned and the number of reads which match to this sequence.
-    # Since all the duplicated sequences were merged, this column can be used as an identifier
-#    df = df.drop_duplicates(subset="counts_sequence", keep="first")
-
-    # now I want to separate sequence and the number of reads
     if len(df) > 0:
-        df[['counts', 'sequence']] = df.get('counts_sequence').str.split('_', expand=True)
         # should be just 3 genes, so not a big deal for performance
         for row_id, row in rrna_genes_df.iterrows():
-            print(row['gene_name'])            
-            #gene_name = row.get('gene_name')
             # now we extract from df only the hits which belong to this gene
             # we check that the start and end position of the hit is between start and end position of the gene
-            gene_df = df.loc[(df.get('chrom') == row.get('chrom')) & (df.get('start') >= row.get('start')) & (df.get('end') <= row.get('end'))]
-            print(len(gene_df))
-            # now we can remove duplicates. Assuming there is no overlap betweeen the regions of rRNA genes, we can safely remove the duplicates so that each sequence will only be counted once
-            # gene_df = gene_df.drop_duplicates(subset="counts_sequence", keep="first")
+            gene_df = df.loc[df.get('gene') == row.get('gene_name')] #  & (df.get('start') >= row.get('start')) & (df.get('end') <= row.get('end'))]
             gene_name = row.get('gene_name')
-            gene_df['gene'] = gene_name
             gene_df['counts'] = gene_df['counts'].astype('int')
-            gene_df = gene_df.sort_values(by=['chrom', 'start', 'end'])
-            gene_df = gene_df.groupby(['chrom','start', 'end', 'gene'],as_index=False).sum() #agg({'counts': 'sum'})
-            output_file = os.path.join(output_dir, '{}_{}_reads_per_positions.txt'.format(sample, gene_name))
+            gene_df = gene_df.sort_values(by=['gene', 'start', 'end'])
+            start_df = gene_df.groupby(['start', 'read_length'], as_index=False).agg({'counts': 'sum'})
+            start_positions = start_df['start'].unique()
+            for start in start_positions:
+                all_reads_info = []
+                for row_id, row in start_df.loc[start_df['start'] == start].iterrows():
+                     all_reads_info.append( "{} reads of length {}".format(row['counts'], row['read_length']))
+                gene_df.loc[gene_df['start'] == start, 'reads_info'] = ",".join(all_reads_info)
+            gene_df = gene_df.groupby(['start'], as_index=False).agg({'counts': 'sum', 'gene': 'first', 'reads_info': 'first'})
+            output_file = os.path.join(output_dir, '{}_{}_reads_per_position.txt'.format(sample, gene_name))
             if len(gene_df) != 0:
                 gene_df.to_csv(output_file, sep="\t", index=False)
                 print("Created file: ", output_file)
