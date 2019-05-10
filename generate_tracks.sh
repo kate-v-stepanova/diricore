957b6c7cd3dc7c933e4769bda1379932116111b1
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-05-10 11:00:56 +0200

    Generate UCSC tracks

--- generate_tracks.sh
+++ generate_tracks.sh
@@ -4,29 +4,34 @@ set -e
 set -u
 
 dataset_id=$1
+if [ "$#" -ge 2 ]; then
+   genome=$2
+else
+   genome="hg19"
+fi
 
 BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
 PROJECT_DIR="$BASE_DIR/$dataset_id"
 DIRICORE_DIR="/home/e984a/diricore"
 
-chrom_sizes="$DIRICORE_DIR/staticdata/human/hg38.chrom.sizes"
+chrom_sizes="$BASE_DIR/static/$genome/$genome.chrom.sizes"
 INDIR="$PROJECT_DIR/analysis/output/tophat_out"
 OUTDIR="$PROJECT_DIR/analysis/output/gen_tracks"
 
 BDG2BW="$DIRICORE_DIR/utils/bdg2bw.sh"
 
 mkdir -p $OUTDIR
-
 # Getting bdg files
 for f in $(ls ${INDIR}); do
   infile="${INDIR}/$f/accepted_hits.hqmapped_dedup.bam"
   echo "Calculating normalization factor for $f"
-
+  samplename=${f#dem_}
+  samplename=${samplename%_umi_extracted}
+  echo $samplename
   fact=$(samtools view -c ${infile}) &&
   fact=$(echo "scale=6; 1000000.0 / $fact" | bc)
-  echo "Done: ${fact}. Writing bdg files: $OUTDIR/${f}_minus.bdg $OUTDIR/${f}_plus.bdg"
-  samtools view -b $infile | tee >(bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand + | sort -k1,1 -k2,2n > $OUTDIR/${f}_plus.bdg) | bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand - | sort -k1,1 -k2,2n > $OUTDIR/${f}_minus.bdg
-
+  echo "Done: ${fact}. Writing bdg files: $OUTDIR/${samplename}_minus.bdg $OUTDIR/${samplename}_plus.bdg"
+  samtools view -b $infile | tee >(bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand + | sort -k1,1 -k2,2n > $OUTDIR/${samplename}_plus.bdg) | bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand - | sort -k1,1 -k2,2n > $OUTDIR/${samplename}_minus.bdg
 done;
 
 # Convert to bigwig
@@ -36,4 +41,3 @@ for f in $(ls ${OUTDIR}/*.bdg); do
   $BDG2BW $f $chrom_sizes
   echo "Done: $(ls ${OUTDIR}/${bn%.*}.bw)"
 done
-
--- utils/bdg2bw.sh
+++ utils/bdg2bw.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+DIRICORE_PATH="/home/e984a/diricore"
+
+# bedClip installed via conda install to diricore environment
+# bedtools are in diricore/programs
+# bedGraphToBigWig also in diricore/programs
+
+
 # check commands: slopBed, bedGraphToBigWig and bedClip
 
 which bedtools &>/dev/null || { echo "bedtools not found! Download bedTools: <http://code.google.com/p/bedtools/>"; exit 1; }
@@ -16,10 +23,10 @@ fi
 F=$1
 G=$2
 
-bedtools slop -i ${F} -g ${G} -b 0 | bedClip stdin ${G} ${F}.clip
+$DIRICORE_PATH/programs/bedtools slop -i ${F} -g ${G} -b 0 | bedClip stdin ${G} ${F}.clip
 
 LC_COLLATE=C sort -k1,1 -k2,2n ${F}.clip > ${F}.sort.clip
 
-bedGraphToBigWig ${F}.sort.clip ${G} ${F/bdg/bw}
+$DIRICORE_PATH/programs/bedGraphToBigWig ${F}.sort.clip ${G} ${F/bdg/bw}
 
 rm -f ${F}.clip ${F}.sort.clip
