7659ee7cd4ab84f5a2a1802d49b002ecde96d371
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-05-23 09:18:43 +0200

    RPF density analysis: minreads in datafile name, don't fail if no file found

--- plot_rpf_transcript_distribution.sh
+++ plot_rpf_transcript_distribution.sh
@@ -13,10 +13,10 @@ minreads=${2};
 genome=$3
 
 OUTDIR="$PROJECT_DIR/analysis/output/figures/rpf_transcript_distribution";
-TX_ALL="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.all.hdf5";
-TX_ALL_DEDUP="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.all.dedup.hdf5";
-TX_HQ="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.hq.hdf5";
-TX_HQ_DEDUP="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.hq.dedup.hdf5";
+TX_ALL="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.all.${minreads}.hdf5";
+TX_ALL_DEDUP="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.all.dedup.${minreads}.hdf5";
+TX_HQ="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.hq.${minreads}.hdf5";
+TX_HQ_DEDUP="$PROJECT_DIR/analysis/output/rpf_5p_density/${project}.txcoord_counts.hq.dedup.${minreads}.hdf5";
 TX_INFO_FILE="$BASE_DIR/static/$genome/transcript_data.hdf5"
 
 DIRICORE_DIR="/home/e984a/diricore"
@@ -24,64 +24,74 @@ python_bin="$DIRICORE_DIR/diricore/bin/plot_rpf_transcript_distribution.py";
 
 mkdir -p ${OUTDIR}
 
-outfile="${OUTDIR}/all.${project}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
-
-sampinfo=`cat $metafile | while read -a LINE
-do
+if [[ -f $TX_ALL ]]; then
+  outfile="${OUTDIR}/all.${project}.${minreads}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
+  sampinfo=`cat $metafile | while read -a LINE
+  do
 	echo -n "'${LINE[0]},${TX_ALL},${LINE[1]},${LINE[2]}' "
-done`
+  done`
 
-eval $(echo python ${python_bin} \
+  eval $(echo python ${python_bin} \
        	-m ${minreads} \
       	-o $outfile \
         -t $TX_INFO_FILE \
 	"$sampinfo")
 
-echo "Done. File created: $outfile"
+  echo "Done. File created: $outfile"
+else
+  echo "$TX_ALL does not exist. Skipping"
+fi
 
-outfile="${OUTDIR}/all.dedup.${project}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
-
-sampinfo=`cat $metafile | while read -a LINE
-do
+if [[ -f $TX_ALL_DEDUP ]]; then
+  outfile="${OUTDIR}/all.dedup.${project}.${minreads}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
+  sampinfo=`cat $metafile | while read -a LINE
+  do
 	echo -n "'${LINE[0]},${TX_ALL_DEDUP},${LINE[1]},${LINE[2]}' "
-done`
+  done`
 
-eval $(echo python ${python_bin} \
+  eval $(echo python ${python_bin} \
        	-m ${minreads} \
       	-o $outfile \
         -t $TX_INFO_FILE \
 	"$sampinfo")
 
-echo "Done. File created: $outfile"
+  echo "Done. File created: $outfile"
+else
+  echo "$TX_ALL_DEDUP does not exist. Skipping"
+fi
 
-
-outfile="${OUTDIR}/hq.${project}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
-
-sampinfo=`cat $metafile | while read -a LINE
-do
+if [[ -f $TX_HQ ]]; then
+  outfile="${OUTDIR}/hq.${project}.${minreads}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
+  sampinfo=`cat $metafile | while read -a LINE
+  do
 	echo -n "'${LINE[0]},${TX_HQ},${LINE[1]},${LINE[2]}' "
-done`
+  done`
 
-eval $(echo python ${python_bin} \
+  eval $(echo python ${python_bin} \
        	-m ${minreads} \
       	-o $outfile \
         -t $TX_INFO_FILE \
 	"$sampinfo")
 
-echo "Done. File created: $outfile"
+  echo "Done. File created: $outfile"
+else
+  echo "$TX_HQ does not exist. Skipping"
+fi
 
-
-outfile="${OUTDIR}/hq.dedup.${project}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
-
-sampinfo=`cat $metafile | while read -a LINE
-do
+if [[ -f $TX_HQ_DEDUP ]]; then
+  outfile="${OUTDIR}/hq.dedup.${project}.${minreads}.rpf_transcript_distribution_plot.${dataset_id}.pdf"
+  sampinfo=`cat $metafile | while read -a LINE
+  do
 	echo -n "'${LINE[0]},${TX_HQ_DEDUP},${LINE[1]},${LINE[2]}' "
-done`
+  done`
 
-eval $(echo python ${python_bin} \
+  eval $(echo python ${python_bin} \
        	-m ${minreads} \
       	-o $outfile \
         -t $TX_INFO_FILE \
 	"$sampinfo")
 
-echo "Done. File created: $outfile"
+  echo "Done. File created: $outfile"
+else
+  echo "$TX_HQ_DEDUP does not exist. Skipping"
+fi
--- run_rpf_density_analysis.sh
+++ run_rpf_density_analysis.sh
@@ -3,9 +3,24 @@
 set -e;
 set -u;
 
-project_id=$1
-species=$2
-minreads=$3
+dataset_id=$1
+BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
+PROJECT_DIR="$BASE_DIR/$dataset_id"
+
+OUTDIR="$PROJECT_DIR/analysis/output/rpf_5p_density";
+INDIR="$PROJECT_DIR/analysis/output/alignments/toGenome";
+PLOTDIR="$PROJECT_DIR/analysis/output/figures";
+SAMPLENAME_FILE="$PROJECT_DIR/analysis/input/metadata/rpf_density_samplenames.tsv";
+CONTRAST_FILE="$PROJECT_DIR/analysis/input/metadata/rpf_density_contrasts.tsv";
+
+species=$2;
+projectname=$dataset_id;
+
+if [[ $# -ge 3 ]]; then
+  minreads=$3
+else
+  minreads=100
+fi
 
 plots_only=0
 if [[ $# -ge 4 ]]; then
@@ -15,47 +30,64 @@ if [[ $# -ge 4 ]]; then
   fi
 fi
 
-BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
-PROJECT_DIR="$BASE_DIR/$project_id"
-OUTDIR="$PROJECT_DIR/analysis/output/rpf_5p_density";
-INDIR="$PROJECT_DIR/analysis/output/tophat_out";
-PLOTDIR="$PROJECT_DIR/analysis/output/figures/rpf_density";
-SAMPLENAME_FILE="$PROJECT_DIR/analysis/input/metadata/rpf_density_samplenames.tsv";
-CONTRAST_FILE="$PROJECT_DIR/analysis/input/metadata/rpf_density_contrasts.tsv";
+DIRICORE_DIR="/home/e984a/diricore"
+INDEXDATA_FILE="$BASE_DIR/static/${species}/transcript_data.hdf5";
+MAPS_FILE="$BASE_DIR/static/${species}/codon_regions.width_61.hdf5";
+MAPSSTART_FILE="$BASE_DIR/static/${species}/codon_regions.START_Other_ATG.width_61.hdf5";
 
-DIRICORE_PATH="/home/e984a/diricore"
-INDEXDATA_FILE="$DIRICORE_PATH/staticdata/${species}/transcript_data.hdf5";
-MAPS_FILE="$DIRICORE_PATH/staticdata/${species}/codon_regions.width_61.hdf5";
-MAPSSTART_FILE="$DIRICORE_PATH/staticdata/${species}/codon_regions.START_Other_ATG.width_61.hdf5";
-
+# Checking that files are present
+ls $INDEXDATA_FILE
+ls $MAPS_FILE
+ls $MAPSSTART_FILE
 
 ###
 # { setup
-of="${OUTDIR}/${project_id}.txcoord_counts.hdf5";
+of_hq_unique="${OUTDIR}/${projectname}.txcoord_counts.hq.dedup.${minreads}.hdf5";
+of_hq="${OUTDIR}/${projectname}.txcoord_counts.hq.${minreads}.hdf5";
+of_all_unique="${OUTDIR}/${projectname}.txcoord_counts.all.dedup.${minreads}.hdf5";
+of_all="${OUTDIR}/${projectname}.txcoord_counts.all.${minreads}.hdf5";
+
 
 mkdir -p ${OUTDIR}
-mkdir -p "${PLOTDIR}"
+mkdir -p "${PLOTDIR}/rpf_5p_density_plots/"
 
 if [[ $plots_only == 0 ]]; then
-  echo "Mapping RPFs to transcriptome coordinates"
-  # map RPFs to transcriptome coordinates
-  $DIRICORE_PATH/diricore/bin/map_rpfs_to_transcriptome_positions.py \
-    -t "${INDEXDATA_FILE}" \
-    -o "${of}" \
-    -b <(\
-        ls -1 ${INDIR}/*.bam | sort -V | while read fn; do
-                b=$(basename "$fn");
-                b=${b%%.*};
+echo "Mapping RPFs to transcripttome coordinates (hq)"
+tmp_file="$OUTDIR/tmp_file.txt"
+rm -f $tmp_file
+touch $tmp_file
+for fn in $(ls ${INDIR}/*.hqmapped.bam); do
+    b=$(basename $fn);
+    b=${b#"star_"};
+    b=${b%%.*};
+    echo -e "${b}\t${fn}" >> $tmp_file
+done;
 
-                echo -e "${b}\t${fn}";
-            done \
-        )
-  # }
-  echo "Done. Created file: $of"
+#$DIRICORE_DIR/diricore/bin/map_rpfs_to_transcriptome_positions.py \
+#   -t "${INDEXDATA_FILE}" \
+#   -o "${of_hq}" \
+#   -b $tmp_file
+
+echo "Mapping done. Created file: $of_hq"
+
+rm -f $tmp_file
+touch $tmp_file
+for fn in $(ls ${INDIR}/*.bam | grep -v "hqmapped"); do
+    b=${b#"star_"};
+    b=${b%%.*};
+    echo -e "${b}\t${fn}" >> $tmp_file
+done;
+
+echo "Mapping RPFs to transcripttome coordinates (all)"
+$DIRICORE_DIR/diricore/bin/map_rpfs_to_transcriptome_positions.py \
+   -t "${INDEXDATA_FILE}" \
+   -o "${of_all}" \
+   -b $tmp_file
+echo "Mapping done. Created file: $of_all"
+rm -f $tmp_file
+
 fi
 
-# { generate RPF density shift plots
-echo "Generating RPF density shift plots"
 output="\
 Ala\tGCA,GCC,GCG,GCT
 Arg\tCGA,CGC,CGG,CGT,AGA,AGG
@@ -79,35 +111,83 @@ Tyr\tTAC,TAT
 Val\tGTA,GTC,GTG,GTT
 "
 
-datafile="${of}";
+# RPF density at special codons
+# (START/other ATG)
 
+echo "Generating RPF density shift plots (hq)"
+mkdir -p $PLOTDIR/rpf_5p_density_plots/hq_with_dup
 echo -ne "$output" | while read aa codongroupstr; do
-    of="${PLOTDIR}/${project_id}.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
+    of="${PLOTDIR}/rpf_5p_density_plots/hq_with_dup/${projectname}.hq.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
     codons=$(echo $codongroupstr | sed 's/,/ /g');
-    python $DIRICORE_PATH/diricore/bin/plot_rpf_5p_density.py \
+
+    python $DIRICORE_DIR/diricore/bin/plot_rpf_5p_density.py \
         -c "${CONTRAST_FILE}" \
         -n "${SAMPLENAME_FILE}" \
         -o "${of}" \
         -m ${minreads} \
-        "${datafile}" \
+        "${of_hq}" \
         ${MAPS_FILE} \
         ${codons}
+    echo "Shift plots done (hq). Created file: $of"
 done
+
 
 # RPF density at special codons
 # (START/other ATG)
+echo "RPF density at special codons (hq)"
 echo -ne "\
 ATG_split\tSTART_ATG,Other_ATG
 " | while read aa codongroupstr; do
-    of="${PLOTDIR}/${project_id}.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
+    of="${PLOTDIR}/rpf_5p_density_plots/hq_with_dup/${projectname}.hq.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
     codons=$(echo $codongroupstr | sed 's/,/ /g');
 
-    python $DIRICORE_PATH/diricore/bin/plot_rpf_5p_density.py \
+    python $DIRICORE_DIR/diricore/bin/plot_rpf_5p_density.py \
         -c "${CONTRAST_FILE}" \
         -n "${SAMPLENAME_FILE}" \
         -o "${of}" \
         -m ${minreads} \
-        "${datafile}" \
+        "${of_hq}" \
         ${MAPSSTART_FILE} \
         ${codons}
+    echo "Special codons done (hq). Created file: $of"
 done
+
+
+echo "Generating RPF density shift plots (all)"
+mkdir -p $PLOTDIR/rpf_5p_density_plots/all_with_dup
+echo -ne "$output" | while read aa codongroupstr; do
+    of="${PLOTDIR}/rpf_5p_density_plots/all_with_dup/${projectname}.all.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
+    codons=$(echo $codongroupstr | sed 's/,/ /g');
+
+    python $DIRICORE_DIR/diricore/bin/plot_rpf_5p_density.py \
+        -c "${CONTRAST_FILE}" \
+        -n "${SAMPLENAME_FILE}" \
+        -o "${of}" \
+        -m ${minreads} \
+        "${of_all}" \
+        ${MAPS_FILE} \
+        ${codons}
+echo "Shift plots done (all). Created file: $of"
+done
+
+
+# RPF density at special codons
+# (START/other ATG)
+echo "RPF density at special codons (all)"
+echo -ne "\
+ATG_split\tSTART_ATG,Other_ATG
+" | while read aa codongroupstr; do
+    of="${PLOTDIR}/rpf_5p_density_plots/all_with_dup/${projectname}.all.m${minreads}.${aa}.rpf_5p_density_shift_plot.pdf";
+    codons=$(echo $codongroupstr | sed 's/,/ /g');
+
+    python $DIRICORE_DIR/diricore/bin/plot_rpf_5p_density.py \
+        -c "${CONTRAST_FILE}" \
+        -n "${SAMPLENAME_FILE}" \
+        -o "${of}" \
+        -m ${minreads} \
+        "${of_all}" \
+        ${MAPSSTART_FILE} \
+        ${codons}
+    echo "Special codons done (all). Created file: $of"
+done
+
