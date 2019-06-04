7dfa8a4b3941b2d0e6f3e80c6078d5e89e013e96
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-06-04 11:24:26 +0200

    align_to_transcriptome: can use any genome version (pre-built index required)

--- align_to_transcriptome.sh
+++ align_to_transcriptome.sh
@@ -4,24 +4,20 @@ set -e
 set -u
 
 dataset_id=$1
+genome=$2
 
 BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
 PROJECT_DIR="$BASE_DIR/$dataset_id"
 INDIR="$PROJECT_DIR/analysis/input/fastq"
-STAR_GENOME_DIR="/icgc/dkfzlsdf/analysis/OE0532/star/star_index"
-
-if [[ "$#" -eq 2 ]]; then
-    OUTDIR=$2
-else
-    OUTDIR=$PROJECT_DIR/analysis/output/alignments
-fi
+STAR_GENOME_DIR="/icgc/dkfzlsdf/analysis/OE0532/static/$genome"
+OUTDIR=$PROJECT_DIR/analysis/output/alignments
 
 echo "Unzip the fastq files in $INDIR to proceed"
 
 module load STAR
 mkdir -p $OUTDIR
 
-for f in $(ls ${INDIR}/*.fastq); do
+for f in $(ls ${INDIR}/*.fastq.gz); do
     b=$(basename ${f});
     b=${b%%.*};
     STAR --genomeDir $STAR_GENOME_DIR \
@@ -29,25 +25,68 @@ for f in $(ls ${INDIR}/*.fastq); do
 	--readFilesIn ${f} \
 	--outFileNamePrefix $OUTDIR/star_${b}_ \
 	--outSAMtype BAM Unsorted \
-        --quantMode TranscriptomeSAM
+        --quantMode TranscriptomeSAM \
+        --readFilesCommand zcat
 done;
 
+echo "Sorting bam files"
+module load samtools/1.6
+for f in $(ls ${OUTDIR}/*.bam); do
+    b=$(basename ${f});
+    b=${b%%.bam}
+    sorted=$OUTDIR/$b.sorted.bam
+    samtools sort $f -o $sorted
+    mv $sorted $f
+done;
 
+mkdir -p $OUTDIR/toTranscriptome
+mkdir -p $OUTDIR/toGenome
 for f in $(ls ${OUTDIR}/*Aligned.toTranscriptome.out.bam); do
     b=$(basename ${f});
     b=${b%%_umi_extract*}; # will get a sample name: GFP_IP_RNAse_3X
     b=${b#*star_dem_};
     b=${b%%_Aligned.toTranscriptome*}; # will get star_dem_GFP_IP_RNAse_3X_umi_extracted
-    mv $f ${OUTDIR}/${b}_toTranscriptome.bam 
+    echo "mv $f ${OUTDIR}/toTranscriptome/${b}_toTranscriptome.bam"
+    mv $f ${OUTDIR}/toTranscriptome/${b}_toTranscriptome.bam 
 done;
 
-echo "Done with alignments; now filtering out non-primary/low-quality alignments";
+for f in $(ls ${OUTDIR}/*Aligned.out.bam); do
+    b=$(basename $f);
+    b=${b%%_umi_extract*}
+    b=${b#*star_dem_}
+    b=${b%%_Aligned.out.bam}
+    echo "mv $f ${OUTDIR}/toGenome/${b}_toGenome.bam"
+    mv $f ${OUTDIR}/toGenome/${b}_toGenome.bam
+done
+
+echo "Done with alignments; now filtering out non-primary/low-quality alignments (for Transciptome files)";
 # isolate "hqmapped" reads
-ls -1 ${OUTDIR}/*toTranscriptome.bam | while read fn; do
+ls -1 ${OUTDIR}/toTranscriptome/*toTranscriptome.bam | while read fn; do
     echo "Processing $fn "
     b=$(basename ${fn})
     b=${b%%".bam"}
-    of="${OUTDIR}/${b}.hqmapped.bam"
+    of="${OUTDIR}/toTranscriptome/${b}.hqmapped.bam"
+#    dn=$(dirname "$fn");
+#    of="${dn}/accepted_hits.hqmapped.bam";
+    cat \
+        <(samtools view -H "${fn}") \
+        <(cat "${fn}" | samtools view -q10 -F260 -) \
+    | samtools view -bS - \
+    > "${of}";
+
+  #  samtools sort $fn > "${OUTDIR}/${b}.sorted.bam"
+  #  samtools sort $of > "${OUTDIR}/${b}.hqmapped.sorted.bam"
+
+done
+echo "Done filtering";
+
+echo "Done with alignments; now filtering out non-primary/low-quality alignments (for Genome files)";
+# isolate "hqmapped" reads
+ls -1 ${OUTDIR}/toGenome/*toGenome.bam | while read fn; do
+    echo "Processing $fn "
+    b=$(basename ${fn})
+    b=${b%%".bam"}
+    of="${OUTDIR}/toGenome/${b}.hqmapped.bam"
 #    dn=$(dirname "$fn");
 #    of="${dn}/accepted_hits.hqmapped.bam";
     cat \
