0d545edf302676647d205b57442ba1d4b763e10a
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-07-03 11:43:20 +0200

    Periodicity: write data to tsv, symlinks to input/bam, create output dirs, ...

--- periodicity.r
+++ periodicity.r
@@ -3,6 +3,7 @@
 library(riboWaltz)
 library(tools)
 library(ggplot2)
+library(data.table)
 
 print("DO NOT FORGET to sort bamfiles with the command: samtools sort sample.bam -o sample.sorted.bam")
 
@@ -10,47 +11,52 @@ args = commandArgs(trailingOnly=TRUE)
 project_id=args[1]
 genome=args[2]
 
+project_id = "3192"
 base_dir = "/icgc/dkfzlsdf/analysis/OE0532"
 project_dir = paste(base_dir, project_id, sep="/")
 static_dir = paste(base_dir, "static", genome, sep="/")
 transcriptome_fasta = paste(static_dir, "transcripts.fa", sep="/")
 contrasts_file = paste(project_dir, "analysis/input/metadata/rpf_density_contrasts.tsv", sep="/")
 
-gtf_file = paste(static_dir, "gencode.v30.basic.annotation.gtf", sep="/")
+gtf_file = paste(static_dir, "gencode.annotation.gtf", sep="/")
 
-bamfolder = paste(project_dir, "analysis/output/alignments_unsorted/periodicity", sep="/")
+bamfolder = paste(project_dir, "analysis/input/periodicity_bam", sep="/")
 plot_dir = paste(project_dir, "analysis/output/figures/periodicity", sep="/")
+data_dir = paste(project_dir, "analysis/output/periodicity", sep="/")
 
-heatmaps = paste(project_dir, "analysis/output/figures/periodicity/heatmap", sep="/")
+heatmaps = paste(plot_dir, "heatmap", sep="/")
+frames_path = paste(plot_dir, "frames", sep="/")
+metaprofile_path = paste(plot_dir, "metaprofile", sep="/")
+#codon_usage_path = paste(plot_dir, "codon_usage", sep="/")
 
-p_sites_per_region = paste(project_dir, "analysis/output/figures/periodicity/p_sites_per_region", sep="/")
-frames_path = paste(project_dir, "analysis/output/figures/periodicity/frames", sep="/")
-metaprofile_path = paste(project_dir, "analysis/output/figures/periodicity/metaprofile", sep="/")
-codon_usage_path = paste(project_dir, "analysis/output/figures/periodicity/codon_usage", sep="/")
+print("Creating output directories")
+dir.create(heatmaps)
+dir.create(frames_path)
+dir.create(metaprofile_path)
 
+samplenames = c()
+for (filename in list.files(bamfolder)) {
+  samplename = file_path_sans_ext(filename)
+  samplenames <- c(samplenames, samplename)
+}
+
+print("Loading input files into memory")
 annotation_dt = create_annotation(gtf_file)
-
 reads_list <- bamtolist(bamfolder = bamfolder, annotation = annotation_dt)
 
 print("P-site offset")
 psite_offset <- psite(reads_list, plot=F, plot_dir=plot_dir)
 reads_psite_list <- psite_info(reads_list, psite_offset)
 
-bam_pattern = "*.hqmapped.bam"
 print("Read ends heatmap")
-for (filename in list.files(bamfolder, pattern=bam_pattern)) {
+for (filename in list.files(bamfolder)) {
   print(filename)
   samplename = file_path_sans_ext(filename)
   ends_heatmap <- rends_heat(reads_list, annotation_dt, sample = samplename, cl = 85, utr5l = 25, cdsl = 40, utr3l = 25)
+  output_file = paste(data_dir, "/", samplename, ".heatmap.tsv", sep="")
+  write.table(ends_heatmap[["dt"]], file=output_file, sep="\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
   plot = ends_heatmap[["plot"]] + scale_colour_gradient2(low="#bfbfbf", mid="#333333", high="black")
   ggsave(paste(heatmaps, "/", samplename, ".jpeg", sep=""), plot=plot, width=15, height=7, units="in")
-}
-
-print("P-site region")
-for (filename in list.files(bamfolder, pattern="*.bam")) {
-  samplename = file_path_sans_ext(filename)
-  psite_region <- region_psite(reads_psite_list, annotation_dt, sample = samplename)
-  ggsave(paste(p_sites_per_region, "/", samplename, ".jpeg", sep=""), plot=psite_region[["plot"]], width=10, height=7, units="in")
 }
 
 print("P-site frames")
@@ -77,23 +83,8 @@ for (filename in list.files(bamfolder, pattern="*.bam")) {
   metaprofile <- metaprofile_psite(reads_psite_list, annotation_dt, sample = samplename,
                                            utr5l = 20, cdsl = 40, utr3l = 20,
                                            plot_title = "auto")
-  ggsave(output_file, device = NULL, plot=metaprofile[["plot"]], width=10, height=7, units="in")
-}
-
-print("Metaprofile heatmap")
-for (filename in list.files(bamfolder, pattern="*.bam")) {
-  samplename = file_path_sans_ext(filename)
-  output_file = paste(metaprofile_path, "/", "heatmap_", samplename, ".jpeg", sep="")
   
-  comparison_dt <- list()
-  subsample = paste(samplename, "28nt", sep="_")
-  comparison_dt[[subsample]] <- reads_psite_list[[samplename]][length == 28]
-  comparison_dt[["whole_sample"]] <- reads_psite_list[[samplename]]
-  names_list <- list("Only_28" = c(subsample),
-                     "All" = c("whole_sample"))
-  metaheatmap <- metaheatmap_psite(comparison_dt, annotation_dt, sample = names_list,
-                                   utr5l = 20, cdsl = 40, utr3l = 20, log = F)
-  ggsave(output_file, device = NULL, plot=metaheatmap[["plot"]], width=10, height=7, units="in")
+  ggsave(output_file, device = NULL, plot=metaprofile[["plot"]], width=10, height=7, units="in")
 }
 
 # print("Codon usage barplot")
