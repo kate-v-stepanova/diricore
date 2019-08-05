4ca469ae318989a728cc2481e8c3e5c30ae66ac4
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-08-05 18:10:34 +0200

    Mutliple cutadapt stats (if samples were dmxed, but not trimmed)

---
+++ utils/multiple_cutadapt_stats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -u
+
+dataset_id=$1
+
+BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
+project_dir="$BASE_DIR/$dataset_id"
+#cutadapt_file="$project_dir/analysis/output/cutadapt_trimming_stats.txt"
+stats_file="$project_dir/analysis/output/cutadapt_plot_stats.txt"
+tmp_file1="$project_dir/analysis/output/tmp_file1.txt"
+tmp_file2="$project_dir/analysis/output/tmp_file2.txt"
+
+rm -f $tmp_file1
+rm -f $tmp_file2
+for f in $(ls $project_dir/analysis/output/S1*cutadapt_trimming_stats.txt); do
+     if [[ ! -f $tmp_file1 ]]; then
+         awk '/Total reads processed/,/Reads written/' $f | cut -d ":" -f2 | cut -d "(" -f1 | tr -d , | tr -d " " | tr -d "\t"> $tmp_file1
+     else
+         awk '/Total reads processed/,/Reads written/' $f | cut -d ":" -f2 | cut -d "(" -f1 | tr -d , | tr -d " " | tr -d "\t" > $tmp_file2
+         paste -d+ $tmp_file1 $tmp_file2 | bc > $stats_file
+         cp $stats_file $tmp_file1
+     fi
+done
+echo -e "Total reads:\nWith adapters:\nToo short:\nPassed filters:" > ${tmp_file2}
+paste ${tmp_file2} $tmp_file1 > $stats_file
+
+rm -f $tmp_file1
+rm -f $tmp_file2
+
+echo "Created file:  $stats_file"
