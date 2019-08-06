27ba73814221257cbbf86402057ad78c7e66ddc8
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-08-06 18:40:56 +0200

    UCSC tracks: can choose bam_type

--- generate_ucsc_tracks.sh
+++ generate_ucsc_tracks.sh
@@ -1,38 +1,59 @@
 #!/bin/bash
 
-set -e;
-set -u;
+set -e
+set -u
 
-export project=$1;
-export cores=${2:-1};
-export OUTDIR="./data/output/ucsc_tracks/";
-export INDIR="./data/output/tophat_out/";
-export METAFILE="./data/input/metadata/ucsc_track_samplenames.tsv";
-export bedtools_bin="./programs/bedtools"
+dataset_id=$1
+if [ "$#" -ge 2 ]; then
+   genome=$2
+else
+   genome="hg19"
+fi
+
+bam_type="hq_unique"
+bam_pattern="_toGenome.hqmapped_dedup.bam"
+# can be: hq, hq_unique, all, all_unique
+if [ $# -ge 3 ]; then
+   bam_type=$3
+fi
+
+if [[ $bam_type == "hq" ]]; then
+   bam_pattern="_toGenome.hqmapped.bam"
+elif [[ $bam_type == "all" ]]; then
+   bam_pattern="_toGenome.bam"
+elif [[ $bam_type == "all_unique" ]]; then
+   bam_pattern="_toGenome_dedup.bam"
+fi
+
+BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
+PROJECT_DIR="$BASE_DIR/$dataset_id"
+DIRICORE_DIR="/home/e984a/diricore"
+
+chrom_sizes="$BASE_DIR/static/$genome/$genome.chrom.sizes"
+#INDIR="$PROJECT_DIR/analysis/output/tophat_out"
+INDIR="$PROJECT_DIR/analysis/output/alignments/toGenome"
+OUTDIR="$PROJECT_DIR/analysis/output/gen_tracks/$bam_type"
+
+BDG2BW="$DIRICORE_DIR/utils/bdg2bw.sh"
 
 
-mkdir ${OUTDIR} || true;
+mkdir -p $OUTDIR
+# Getting bdg files
+for f in $(ls $INDIR/*"$bam_pattern"); do
+  echo "Calculating normalization factor for $f"
+  samplename=$(basename $f)
+  samplename=${samplename%"$bam_pattern"}
+  echo $samplename
+  fact=$(samtools view -c $f) &&
+  fact=$(echo "scale=6; 1000000.0 / $fact" | bc)
+  echo "Done: ${fact}. Writing bdg files: $OUTDIR/${samplename}_minus.bdg $OUTDIR/${samplename}_plus.bdg"
+  samtools view -b $f | tee >(bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand + | sort -k1,1 -k2,2n > $OUTDIR/${samplename}_plus.bdg) | bedtools genomecov -ibam /dev/stdin -g $chrom_sizes -scale $fact -bg -split -strand - | sort -k1,1 -k2,2n > $OUTDIR/${samplename}_minus.bdg
+done;
 
-
-
-bamtobed(){
-	sampname=$(echo $1 | cut -d ' ' -f1)
-	samplabel=$(echo $1 | cut -d ' ' -f2)
-	sampcol=$(echo $1 | cut -d ' ' -f3)
-	samppath="${INDIR}/${sampname}/accepted_hits.hqmapped.bam"
-	for strand in "+" "-"; do
-	trackopt="name=${samplabel}_${strand} visibility=2 color=${sampcol} maxHeightPixels=50"
-	outf="${OUTDIR}/${project}_${sampname}_${samplabel}_${strand}.bdg.gz"
-	${bedtools_bin} genomecov \
-	-ibam ${samppath} -bg -split \
-	-strand "${strand}" \
-	-trackline -trackopts "${trackopt}" \
-	| gzip > ${outf}
-	echo "Done with file: ${outf}"
-	done
-	}
-export -f bamtobed
-
-echo "Preparing UCSC tracks..."
-cat $METAFILE | parallel bamtobed
-echo "Done"
+# Convert to bigwig
+for f in $(ls ${OUTDIR}/*.bdg); do
+  bn=$(basename ${f})
+  echo "Converting to bigwig "
+  $BDG2BW $f $chrom_sizes
+  echo "Done: $(ls ${OUTDIR}/${bn%.*}.bw)"
+done
