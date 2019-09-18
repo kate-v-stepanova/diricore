5442bd3f634dbb0bd4521e0083078c5456e5e5b6
E.Stepanova <kate-v-stepanova@github.com>
2019-09-18 18:01:13 +0200

    Deduplicate

--- deduplicate_umi.sh
+++ deduplicate_umi.sh
@@ -4,47 +4,75 @@ set -e;
 set -u;
 
 dataset_id=$1
+genome_dir="toGenome"
+if [[ $# -ge 3 ]]; then
+   if [[ $3 == "--transcriptome" ]]; then
+       genome_dir="toTranscriptome"
+   elif [[ $3 == "--toTranscriptome" ]]; then
+       genome_dir="toTranscriptome"
+   elif [[ $3 == "--totranscriptome" ]]; then
+       genome_dir="toTranscriptome"
+   elif [[ $3 == "--trans" ]]; then
+       genome_dir="toTranscriptome"
+   elif [[ $3 == "trans" ]]; then
+       genome_dir="toTranscriptome"
+   fi
+fi
+echo $genome_dir    
+
+bam_type="hq"
+# can be: hq, all
+if [[ $# -ge 2 ]]; then
+   if [[ $2 == "all" ]]; then
+       bam_type="all"
+   fi
+fi 
+echo $bam_type
 
 BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
 PROJECT_DIR="$BASE_DIR/$dataset_id"
-INDIR="$PROJECT_DIR/analysis/output/tophat_out"
-OUTDIR="$PROJECT_DIR/analysis/output/tophat_out"
+INDIR="$PROJECT_DIR/analysis/output/alignments/$genome_dir"
+OUTDIR="$PROJECT_DIR/analysis/output/alignments/$genome_dir"
 
-## HQ mapped reads
-echo "HQ mapped reads"
-# index
-for d in $(ls ${INDIR}); do
-    f="${INDIR}/${d}/accepted_hits.hqmapped.bam";
-    outfile="${OUTDIR}/${d}/accepted_hits.hqmapped.bam.bai";
+mkdir -p $OUTDIR/logs
+
+
+if [[ $bam_type == "hq" ]]; then
+  ## HQ mapped reads
+  echo "HQ mapped reads"
+  # index
+  for f in $(ls ${INDIR}/*.hqmapped.bam); do
+    samplename=$(basename $f);
+    samplename=${samplename%".hqmapped.bam"}
+    outfile="${OUTDIR}/${samplename}.hqmapped.bam.bai";
     echo "Indexing ${f}";
     samtools index ${f}
-done;
+  done;
 
-for d in $(ls ${INDIR}); do
-    f="${INDIR}/${d}/accepted_hits.hqmapped.bam";
+  for f in $(ls ${INDIR}/*.hqmapped.bam); do
+    samplename=$(basename $f)
+    samplename=${samplename%".bam"}
     echo "Deduplicating ${f}";
-    fb=$(basename ${f});
-    fb=${fb%.*};
-    umi_tools dedup --log2stderr -L ${OUTDIR}/${d}/${fb}_umidedup_log.txt -I ${f} -S ${OUTDIR}/${d}/${fb}_dedup.bam --output-stats=${OUTDIR}/${d}/${fb}_dedup.log
-done;
+    umi_tools dedup --log2stderr -L ${OUTDIR}/logs/${samplename}_umidedup_log.txt -I ${f} -S ${OUTDIR}/${samplename}_dedup.bam --output-stats=${OUTDIR}/logs/${samplename}_dedup.log
+  done;
+fi
 
-
-## ALL reads
-echo "ALL reads"
-for d1 in $(ls ${INDIR}); do
-    f="${INDIR}/${d1}/accepted_hits.bam";
-    outfile="${OUTDIR}/${d1}/accepted_hits.bam.bai";
+if [[ $bam_type == "all" ]]; then
+  ## All reads
+  echo "All reads"
+  # index
+  for f in $(ls ${INDIR}/*${genome_dir}.bam); do
+    samplename=$(basename $f);
+    samplename=${samplename%"${genome_dir}.bam"}
+    outfile="${OUTDIR}/${samplename}_${genome_dir}.bam.bai";
     echo "Indexing ${f}";
     samtools index ${f}
-done;
+  done;
 
-for d1 in $(ls ${INDIR}); do
-   if [[ "$d1" != "logs" ]]; then 
-        echo $d1;
-	f1="${INDIR}/${d1}/accepted_hits.bam";
-        echo "Deduplicating ${f1}";
-        fb1=$(basename ${f1});
-        fb1=${fb1%.*};
-        umi_tools dedup --log2stderr -L ${OUTDIR}/${d1}/${fb1}_umidedup_log.txt -I ${f1} -S ${OUTDIR}/${d1}/${fb1}_dedup.bam --output-stats=${OUTDIR}/${d1}/${fb1}_dedup.log
-    fi
-done;
+  for f in $(ls ${INDIR}/*${genome_dir}.bam); do
+    samplename=$(basename $f)
+    samplename=${samplename%".bam"}
+    echo "Deduplicating ${f}";
+    umi_tools dedup --log2stderr -L ${OUTDIR}/logs/${samplename}_umidedup_log.txt -I ${f} -S ${OUTDIR}/${samplename}_dedup.bam --output-stats=${OUTDIR}/logs/${samplename}_dedup.log
+  done;
+fi
