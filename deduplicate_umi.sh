ad3e9faecf7572756549417b8d1f19162f87b8aa
E.Stepanova <kate-v-stepanova@github.com>
2019-09-20 17:13:08 +0200

    Deduplication: run all samples in parallel

--- deduplicate_umi.sh
+++ deduplicate_umi.sh
@@ -4,6 +4,7 @@ set -e;
 set -u;
 
 dataset_id=$1
+project_id=$dataset_id
 genome_dir="toGenome"
 if [[ $# -ge 3 ]]; then
    if [[ $3 == "--transcriptome" ]]; then
@@ -15,6 +16,8 @@ if [[ $# -ge 3 ]]; then
    elif [[ $3 == "--trans" ]]; then
        genome_dir="toTranscriptome"
    elif [[ $3 == "trans" ]]; then
+       genome_dir="toTranscriptome"
+   elif [[ $3 == "toTranscriptome" ]]; then
        genome_dir="toTranscriptome"
    fi
 fi
@@ -33,46 +36,43 @@ BASE_DIR="/icgc/dkfzlsdf/analysis/OE0532"
 PROJECT_DIR="$BASE_DIR/$dataset_id"
 INDIR="$PROJECT_DIR/analysis/output/alignments/$genome_dir"
 OUTDIR="$PROJECT_DIR/analysis/output/alignments/$genome_dir"
+script_dir="/icgc/dkfzlsdf/analysis/OE0532/tmp/$project_id/deduplicating"
 
+mkdir -p $script_dir
 mkdir -p $OUTDIR/logs
 
 
 if [[ $bam_type == "hq" ]]; then
-  ## HQ mapped reads
-  echo "HQ mapped reads"
-  # index
-  for f in $(ls ${INDIR}/*.hqmapped.bam); do
-    samplename=$(basename $f);
-    samplename=${samplename%".hqmapped.bam"}
-    outfile="${OUTDIR}/${samplename}.hqmapped.bam.bai";
-    echo "Indexing ${f}";
-    samtools index ${f}
-  done;
-
-  for f in $(ls ${INDIR}/*.hqmapped.bam); do
-    samplename=$(basename $f)
-    samplename=${samplename%".bam"}
-    echo "Deduplicating ${f}";
-    umi_tools dedup --log2stderr -L ${OUTDIR}/logs/${samplename}_umidedup_log.txt -I ${f} -S ${OUTDIR}/${samplename}_dedup.bam --output-stats=${OUTDIR}/logs/${samplename}_dedup.log
-  done;
+    bam_pattern="_${genome_dir}.hqmapped.bam"
+    bam_prefix="_${genome_dir}.hqmapped_dedup.bam"
+else
+    bam_pattern="_${genome_dir}.bam"
+    bam_prefix="_${genome_dir}_dedup.bam"
 fi
 
-if [[ $bam_type == "all" ]]; then
-  ## All reads
-  echo "All reads"
-  # index
-  for f in $(ls ${INDIR}/*${genome_dir}.bam); do
+for f in $(ls ${INDIR}/*$bam_pattern); do
     samplename=$(basename $f);
-    samplename=${samplename%"${genome_dir}.bam"}
-    outfile="${OUTDIR}/${samplename}_${genome_dir}.bam.bai";
-    echo "Indexing ${f}";
-    samtools index ${f}
-  done;
+    samplename=${samplename%"$bam_pattern"}
+    script_file="$script_dir/${samplename}${bam_pattern}.sh"
+    ind_file="${OUTDIR}/${samplename}${bam_pattern}.bai";
+    if [[ ! -f $ind_file ]]; then
+        echo "samtools index ${f}" > $script_file
+    fi
+    outfile=${OUTDIR}/${samplename}${bam_prefix}
+    if [[ ! -f $outfile ]]; then
+      samplename=$(basename $f)
+      samplename=${samplename%".bam"}
+      echo "umi_tools dedup --log2stderr -L ${OUTDIR}/logs/${samplename}_umidedup_log.txt -I ${f} -S $outfile --output-stats=${OUTDIR}/logs/${samplename}_dedup.log" >> $script_file
+      chmod +x $script_file
+    fi
+done;
 
-  for f in $(ls ${INDIR}/*${genome_dir}.bam); do
-    samplename=$(basename $f)
-    samplename=${samplename%".bam"}
-    echo "Deduplicating ${f}";
-    umi_tools dedup --log2stderr -L ${OUTDIR}/logs/${samplename}_umidedup_log.txt -I ${f} -S ${OUTDIR}/${samplename}_dedup.bam --output-stats=${OUTDIR}/logs/${samplename}_dedup.log
-  done;
+if ls $script_dir/*${bam_pattern}.sh 1> /dev/null 2>&1; then
+#if [[ -f $script_dir/*${bam_pattern}.sh ]]; then
+    for f in $(ls $script_dir/*${bam_pattern}.sh); do
+        echo "bsub -q medium $f"
+    done
+else
+    echo "Files already exist"
+    ls $OUTDIR/*${bam_pattern}
 fi
