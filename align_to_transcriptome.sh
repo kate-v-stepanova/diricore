ec42b50d9b1a89bd295b644eb4940e8e1f4de164
E.Stepanova <kate-v-stepanova@github.com>
2020-08-03 14:27:58 +0200

    Alignment script update

--- align_to_transcriptome.sh
+++ align_to_transcriptome.sh
@@ -29,25 +29,28 @@ for f in $(ls ${INDIR}/*.fastq.gz); do
     echo "set -e" >> $outfile # exit on error, don't continue
     # align
     echo "module load STAR" >> $outfile
-    echo "STAR --genomeDir $STAR_GENOME_DIR --runThreadN 100 --readFilesIn ${f} --outFileNamePrefix $OUTDIR/${b}_ --outSAMtype BAM Unsorted --readFilesCommand zcat  --quantMode TranscriptomeSAM GeneCounts" >> $outfile
+    echo "STAR --genomeDir $STAR_GENOME_DIR --runThreadN 100 --readFilesIn ${f} --outFileNamePrefix $OUTDIR/${b}_ --outSAMtype BAM Unsorted --readFilesCommand zcat  --quantMode TranscriptomeSAM GeneCounts --outSAMmapqUnique 0" >> $outfile
 	# sort transcriptome
-	echo "module load samtools/1.6" >> $outfile
-	sorted="$OUTDIR/$b.sorted.trans.bam"
+    echo "mv $OUTDIR/${b}_Aligned.toTranscriptome.out.bam ${OUTDIR}/toTranscriptome/${b}_toTranscriptome.bam" >> $outfile
     # samtools sort: option -T is important!! this is where the tmp will be written. Otherwise there will be problems with:
 	#  1) running 2 datasets with equal sample names, the bam files will be mixed up!
     #  2) hard drive quota will be exceeded.
-	echo "samtools sort $OUTDIR/${b}_Aligned.toTranscriptome.out.bam -o $sorted -T $OUTDIR/$b" >> $outfile
-	echo "mv $sorted $OUTDIR/${b}_Aligned.toTranscriptome.out.bam" >> $outfile
-	echo "mv $OUTDIR/${b}_Aligned.toTranscriptome.out.bam ${OUTDIR}/toTranscriptome/${b}_toTranscriptome.bam" >> $outfile
+    echo "module load samtools/1.6" >> $outfile
+	sorted="$OUTDIR/toTranscriptome/$b.sorted.trans.bam"
+    echo "samtools sort $OUTDIR/toTranscriptome/${b}_toTranscriptome.bam -o $sorted -T $OUTDIR/trans.$b" >> $outfile
+	echo "mv $sorted $OUTDIR/toTranscriptome/${b}_toTranscriptome.bam" >> $outfile
+
 	# sort genome
-	sorted="$OUTDIR/$b.sorted.bam"
-	echo "samtools sort $OUTDIR/${b}_Aligned.out.bam -o $sorted -T $OUTDIR/$b" >> $outfile
-	echo "mv $sorted ${b}_Aligned.out.bam" >> $outfile
 	echo "mv ${OUTDIR}/${b}_Aligned.out.bam ${OUTDIR}/toGenome/${b}_toGenome.bam" >> $outfile
+	sorted="$OUTDIR/toGenome/$b.sorted.bam"
+	echo "samtools sort $OUTDIR/toGenome/${b}_toGenome.bam -o $sorted -T $OUTDIR/gen.$b" >> $outfile
+	echo "mv $sorted $OUTDIR/toGenome/${b}_toGenome.bam" >> $outfile
+
 	# mv log files
-	echo "mv $OUTDIR/${b}*ReadsPerGene.out.tab $OUTDIR/reads_per_gene" >> $outfile
+	echo "mv $OUTDIR/${b}*ReadsPerGene.out.tab $OUTDIR/reads_per_gene/" >> $outfile
 	echo "mv $OUTDIR/${b}*.out $OUTDIR/logs/" >> $outfile
 	echo "mv $OUTDIR/${b}*.tab $OUTDIR/logs/" >> $outfile
+
 	## extract hq reads
 	gen_bam="$OUTDIR/toGenome/${b}_toGenome.bam"
 	gen_hq_bam="$OUTDIR/toGenome/${b}_toGenome.hqmapped.bam"
@@ -57,6 +60,6 @@ for f in $(ls ${INDIR}/*.fastq.gz); do
 	echo "cat <(samtools view -H $trans_bam)  <(cat $trans_bam | samtools view -q10 -F260 -) | samtools view -bS - > $trans_hq_bam " >> $outfile
 	
 	chmod +x $outfile
-	echo "bsub -q long $outfile"
+	echo "bsub -q long  -R \"rusage[mem=7G]\" $outfile"
 done
 
